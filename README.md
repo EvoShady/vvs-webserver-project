c43d232223ef2ff1d98536ed69ef10f337671a6a
EvoShady <danc1_cosmin@yahoo.com>
2021-11-07 21:49:00 +0200

    Added some tests for web server

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # VVS-Webserver-Project by Danci Ionut Cosmin
 
 Legend:
+
 ✓ -> done
 
 x -> not done
 
-~ -> somewhat done
+~ -> partially done
 
 # Implementation:
 - Webserver ✓
@@ -17,10 +18,10 @@ x -> not done
 - cliTest ~
 - configTest ~
 - guiTests x
-- WebServerTest x
+- WebServerTest ~
 
-Current code coverage ~24%
-
-![Code_Coverage_Image](./code_coverage.PNG)
-
+Current code coverage: 40.7%
+![Code_Coverage_Image](./current_code_coverage.PNG)
 Status: need help.
+
+Nu am reusit sa fac asa multe teste unitare deoarece nu stiu exact cum ar trebuii sa testez cazurile in care apar metode de multithreading/blocking din codul meu, poate ar trebuii refactorizat si as dorii feedback daca se poate. Multumesc!
--- tests/webserverTests/WebServerTest.java
+++ tests/webserverTests/WebServerTest.java
@@ -1,5 +1,76 @@
 package webserverTests;
 
+import cli.Main;
+import config.Config;
+import org.junit.Before;
+import org.junit.Test;
+import webserver.WebServer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.junit.Assert.assertEquals;
+
 public class WebServerTest {
-    //TODO -> HERE WILL BE THE TESTS FOR WEBSERVER AFTER IMPLEMENTATION
+    WebServer webServer;
+
+    @Before
+    public void setUp() {
+        webServer = new WebServer();
+    }
+
+    @Test
+    public void checkCheckMaintenanceModeIsOn(){
+        webServer.setMaintenanceMode(true);
+        assertEquals(true, webServer.isMaintenanceMode());
+    }
+
+    @Test
+    public void checkCheckMaintenanceModeIsOff(){
+        webServer.setMaintenanceMode(false);
+        assertEquals(false, webServer.isMaintenanceMode());
+    }
+
+    @Test (expected = FileNotFoundException.class)
+    public void checkFile() throws IOException {
+        PrintWriter out = null;
+        OutputStream dataOut = null;
+        String fileRequested = "thisFileDoesNotExist";
+        webServer.fileNotFound(out, dataOut, fileRequested);
+    }
+
+    @Test
+    public void worksWithHtmlTerminatedString(){
+        assertEquals("test.html", webServer.determineServerServingPage("test.html"));
+    }
+
+    @Test
+    public void worksWithNotExtensionTerminatedString(){
+        assertEquals("test2.html", webServer.determineServerServingPage("test2"));
+    }
+
+    @Test
+    public void worksWithJpgs(){
+        assertEquals("test3.jpg", webServer.determineServerServingPage("test3.jpg"));
+    }
+
+    @Test
+    public void worksInMaintenanceMode(){
+        webServer.setMaintenanceMode(true);
+        assertEquals("/maintenance.html", webServer.determineServerServingPage("test4.jpg"));
+    }
+
+    @Test
+    public void worksInDefaultMode(){
+        assertEquals("/index.html", webServer.determineServerServingPage("/"));
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void runServerWithNothing(){
+        webServer.run();
+    }
 }
